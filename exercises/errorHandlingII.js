ae2ec2e09cc7648282fe1d179ac313029e87b3dc
kaoslabs <42324929+kaoslabs@users.noreply.github.com>
2019-05-15 08:57:55 -0500

    write errorHanding code

--- exercises/errorHandlingII.js
+++ exercises/errorHandlingII.js
@@ -1,4 +1,4 @@
-module.exports.run = function(url){
+module.exports.run = async function(url){
 /*
 	You are given a url. Make a GET call with axios using the given url.
 	However, there is something wrong with the url, and it throws an error.
@@ -8,4 +8,20 @@ module.exports.run = function(url){
 
 	Write your code below the comment.
 */
+	// declare vars
+	const axios = require('axios');
+	var message;
+
+	// await axios.get completion before returning message
+	await axios.get(url)
+		// .then(function (response){
+		// 	message = 'No issues here!';
+		// })
+		.catch(function (error){
+			// get error message and append to Error:
+			message = 'Error: ' + error.message;
+		});
+	
+	// return message
+	return message;
 };
