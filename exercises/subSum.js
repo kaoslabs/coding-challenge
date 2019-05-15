b315b28af29180a07d4f0a83812539e789236c80
kaoslabs <42324929+kaoslabs@users.noreply.github.com>
2019-05-14 20:51:39 -0500

    write subSum code

--- exercises/subSum.js
+++ exercises/subSum.js
@@ -11,11 +11,28 @@ module.exports.run = function(x, y) {
 
 	 Write your code below the comment.
 */
-const twoDArray = [[0,  4,  2,  1],
-		   [3, -1,  1,  0],
-		   [0,  0,  2,  3],
-		   [1,  3, -3,  0]];
+	// 2dArr given
+	const twoDArray = [[0,  4,  2,  1],
+					   [3, -1,  1,  0],
+					   [0,  0,  2,  3],
+		   			   [1,  3, -3,  0]];
+	// start sum at 0
+	sum = 0;
 
+	// max x & y out at array dimensions
+	if (x > twoDArray[0].length) x = twoDArray[0].length;
+	if (y > twoDArray.length) y = twoDArray.length;
+
+	// iterate rows and columns up to y and x, respectively
+	for (r = 0; r <= y; r++){
+		for (c = 0; c <= x; c++){
+			// add value to sum
+			sum += twoDArray[r][c];
+		}
+	}
+
+	// return sum
+	return sum;
 };
 
 
